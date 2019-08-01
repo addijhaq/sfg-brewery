f36e326ebb5039949efac3bbe9ceb64a33ed6fce
Ulhaq, Addison <addison.ulhaq@thermofisher.com>
2019-08-01 17:07:27 -0400

    first commit

---
+++ src/test/java/com/addison/appbrewery/SFG/Brewery/SfgBreweryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.addison.appbrewery.SFG.Brewery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgBreweryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
